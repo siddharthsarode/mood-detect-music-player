74e4196cfdf7dfb4d9bcd02b90b886e6f68aac71
Siddharth Sarode <SiddharthSarode279@gmail.com>
2025-07-26 14:00:56 +0530

    change app.js file

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,14 +10,33 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+const allowedOrigins =
+  process.env.NODE_ENV === "development"
+    ? ["http://localhost:5173", "http://localhost:3000"]
+    : ["https://echomood-lqbl.onrender.com"];
+
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === "development"
-        ? ["http://localhost:5173", "http://localhost:3000"]
-        : [process.env.HOST_CLIENT, process.env.HOST_SERVER],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin:
+//       process.env.NODE_ENV === "development"
+//         ? ["http://localhost:5173", "http://localhost:3000"]
+//         : [process.env.HOST_CLIENT, process.env.HOST_SERVER],
+//   })
+// );
 
 app.use("/songs", songRoute);
 

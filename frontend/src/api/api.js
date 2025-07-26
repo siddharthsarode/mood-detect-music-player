8ab99ce194641ff9ab92d9e3743a6814ac9ab37c
Siddharth Sarode <SiddharthSarode279@gmail.com>
2025-07-26 13:43:48 +0530

    and some files and variable

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    import.meta.env.VITE_APP_NODE_ENV !== "development"
+      ? import.meta.env.VITE_APP_BASE_URL
+      : "http://localhost:3000",
 });
 
 export default instance;
